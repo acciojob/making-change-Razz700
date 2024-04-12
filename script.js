b9b6465282cd8fdce3327ca754c63494f25f298d
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-12 11:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const makeChange = (c) => {
   // your name here
+	 let obj={q:0,d:0,n:0,p:0},r;
+  r=(c-c%25)/25;
+  obj.q=r;
+  c=(c-r*25)%25;
+  r=Math.floor(c/10);
+ obj.d=r;
+ c=c%10;
+ r=Math.floor(c/5);
+ obj.n=r;
+ c=c%5;
+ obj.p=c;
+ return obj;
 };
 
 // Do not the change the code below
